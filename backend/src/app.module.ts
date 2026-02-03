acd0c96c7cbb4e57f5b2c7a781eb408b4eee97ac
obrienma <obrien.ma@gmail.com>
2026-02-02 20:46:03 -0800

    Created Prisma Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProvidersModule } from './providers/providers.module';
+import { AppointmentsModule } from './appointments/appointments.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UsersModule, ProvidersModule, AppointmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
